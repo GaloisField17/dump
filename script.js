a4b3945e392f25501d55f63e40090e9ec30315f2
unknown <galoisfield2017sept@gmail.com>
2025-05-02 02:15:35 -0400

    fix js script

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-fetch("https://raw.githubusercontent.com/GaloisField17/dump/refs/heads/main/en-US")
-  .then((res) => res.text())
-  .then((text) => {
-    const lines = text.replace(/"|;/g, '')
+const fetchHeroes = async () => {
+  try {
+    const res = await fetch("https://raw.githubusercontent.com/GaloisField17/dump/refs/heads/main/en-US");
+    const text = await res.text();
+    const heroes = [];
+    text.replace(/"|;/g, '')
       .split('\n')
       .filter((line) => line.startsWith("hero_name_"))
-      .map((line) => {
-        const [key, val] = line.replace(/hero_name_/g, '').split('=')
-        return { id: key, name: val }
+      .forEach((line) => {
+        const [key, val] = line.replace('hero_name_', '').split('=');
+        heroes.push({ id: key.trim(), name: val.trim() });
       });
-  })
-  .catch((e) => console.error(e));
+    return heroes;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
 
+(async () => {
+  const heroes = await fetchHeroes();
+  console.log(heroes);
+})();
