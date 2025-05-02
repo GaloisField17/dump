ee0ee05342c7dd03de41793c450b505b5468c979
unknown <galoisfield2017sept@gmail.com>
2025-05-02 02:34:38 -0400

    added fileNames

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const fetchFiles = async () => {
+  try {
+    const res = await fetch("");
+    const text = await res.text();
+    const files = [];
+    text.split('\n')
+      .filter((line) => line.endsWith(".skel"))
+      .forEach((line) => {
+        const fileName = line.replace('.skel', '');
+        files.push({
+          fileName: fileName,
+          atlas: fileName + ".atlas",
+          png: fileName + ".png",
+          skel: fileName + ".skel"
+        });
+      });
+    return heroes;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
+
 const fetchHeroes = async () => {
   try {
     const res = await fetch("https://raw.githubusercontent.com/GaloisField17/dump/refs/heads/main/en-US");
@@ -8,7 +31,7 @@ const fetchHeroes = async () => {
       .filter((line) => line.startsWith("hero_name_"))
       .forEach((line) => {
         const [key, val] = line.replace('hero_name_', '').split('=');
-        heroes.push({ id: key.trim(), name: val.trim() });
+        heroes.push({ id: key, heroName: val });
       });
     return heroes;
   } catch (e) {
