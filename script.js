5d035d0e03d3c6705310386e0de670de464c6d05
unknown <galoisfield2017sept@gmail.com>
2025-05-02 02:01:49 -0400

    minify js script

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-fetch("C:\Users\Calvin\Desktop\SVtest\abc\en-US")
+const heroes = [];
+fetch("https://raw.githubusercontent.com/GaloisField17/dump/refs/heads/main/en-US")
   .then((res) => res.text())
   .then((text) => {
-    var lines = text.split('\n').filter((line) => line.startsWith("hero_name_"));
-    console.log(lines);
+    const lines = text.replace(/"|;/g, '')
+      .split('\n')
+      .filter((line) => line.startsWith("hero_name_"))
+      .map((line) => {
+        const [key, val] = line.replace(/hero_name_/g, '').split('=')
+        return { id: key, name: val }
+      });
   })
   .catch((e) => console.error(e));
+
